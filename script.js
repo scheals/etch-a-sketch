0d278d8bafaeeebb457c72cdbe5f08a04787fceb
scheals <piecoo@gmail.com>
2022-01-03 20:55:33 +0100

    Implement button and changes to page layout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gridContainer = document.querySelector('main');
+const gridContainer = document.querySelector('.grid');
 for (let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
     div.setAttribute('class', 'square');
