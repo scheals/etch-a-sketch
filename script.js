348f5b2e9894c788527a1ddadd85d9eae0efb013
scheals <piecoo@gmail.com>
2022-01-03 22:36:26 +0100

    Revert "Make layout clearer" - accident.
    This reverts commit caeb6c5e099747ca1dc9731f86e59dc84e52786b.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const gridContainer = document.querySelector('.grid');
-for (let i = 1; i < 256; i++) {
+for (let i = 1; i <= 256; i++) {
     const div = document.createElement('div');
     div.setAttribute('class', 'square');
     gridContainer.appendChild(div);
