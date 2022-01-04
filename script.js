cf88c59f5738672b0fccf48a36b5fc8c71048b31
scheals <piecoo@gmail.com>
2022-01-04 22:01:34 +0100

    Implement random square color on mouseover

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-const button = document.querySelector('.reset-btn')
+const button = document.querySelector('.reset-btn');
 const buttonListener = button.addEventListener('click', function () {resetGrid()});
 createGrid(16);
+squareMagic();
 function resetGrid () {
     const divRow = document.querySelector('.grid');
     while (divRow.firstChild) {
         divRow.removeChild(divRow.firstChild);
     }
-    let squareNumber = +prompt("How many squares per row & column?", "Accepted range: 0-100")
-    return createGrid(squareNumber);
+    let squareNumber = +prompt("How many squares per row & column?", "Accepted range: 0-100");
+    createGrid(squareNumber);
+    squareMagic();
+    return squareNumber;
 }
 
 function createGrid(number) {
@@ -20,7 +23,22 @@ function createGrid(number) {
         for (let i = 1; i <= number; i++) {
             const divSquare = document.createElement('div');
             divSquare.setAttribute('class', 'square');
+            divSquare.setAttribute ('style', 'background-color: black;');
             divRow.appendChild(divSquare);
         }
     }
+}
+
+function squareMagic () {
+    const square = document.querySelectorAll('.square');
+    square.forEach(element => squareListener(element));
+}
+function squareListener(element) {
+    element.addEventListener('mouseover', function() {randomColor(element)});
+}
+function randomColor(element) {
+    let r = Math.floor([Math.random()] * 255);
+    let g = Math.floor([Math.random()] * 255);
+    let b = Math.floor([Math.random()] * 255);
+    return element.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
 }
